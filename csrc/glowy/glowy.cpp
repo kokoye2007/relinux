b243f60575df91f3513a72438d7b0f4072d7e350
MiJyn <mijyn@mail.com>
2012-08-06 16:11:50 -0600

    Sped up the GUI a lot, added a unused C API (might be deleted), and added more components that use the Glowy theme

---
+++ csrc/glowy/glowy.cpp
@@ -0,0 +1,14 @@
+/*
+ * glowy.cpp
+ *
+ *  Created on: 2012-08-04
+ *      Author: MiJyn
+ */
+
+#include <iostream>
+
+void hello()
+{
+	std::cout << "hi" << std::endl;
+	std::cout << "hello" << std::endl;
+}
