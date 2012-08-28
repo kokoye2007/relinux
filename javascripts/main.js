c9f1397973293fd427a84fd68396cf965e83f3d1
Joel <mijyn@mail.com>
2012-08-28 15:02:34 -0600

    Better website (still under heavy progress)

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,45 @@
-console.log('This would be the main JS file.');
+function start()
+{
+	$(".navbar").scrollspy();
+	$('[data-spy="scroll"]').each(function()
+	{
+		$(this).scrollspy('refresh');
+	});
+	$("ul.nav li a").hover(function()
+	{
+		$("#navbar-highlight-hover").show();
+		$("#navbar-highlight-hover").css({
+			"width" : $(this).outerWidth()
+		});
+		$("#navbar-highlight-hover").offset({
+			"left" : $(this).offset().left
+		});
+	}, function()
+	{
+		$("#navbar-highlight-hover").hide();
+	});
+	$("ul.nav li").on("activate", function()
+	{
+		console.log("GOOD");
+	});
+	$("ul.nav li").trigger("activate");
+}
+
+$(document).ready(start);
+
+function showhide(el)
+{
+	hidden = true
+	if (!($(el + " .hideme").hasClass("hidden")))
+	{
+		hidden = false
+	}
+	$(el + " .hideme").toggleClass("hidden");
+	if (hidden)
+	{
+		$(el + " .showhide").html("&laquo; Show less")
+	} else
+	{
+		$(el + " .showhide").html("Show more &raquo;")
+	}
+}
